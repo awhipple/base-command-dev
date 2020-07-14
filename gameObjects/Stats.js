53f363852b439fff0e538f37aec0ab3aba04fb4f
Aaron Whipple <argwhip@gmail.com>
2020-07-14 12:46:11 -0700

    Upgrade leveling arc again

--- gameObjects/Stats.js
+++ gameObjects/Stats.js
@@ -3,12 +3,12 @@ export default {
     lvl: 1,
     val: 1,
     next: (lvl) => lvl + 1,
-    cost: (lvl) => Math.floor(10 * Math.pow(lvl, 2.2)),
+    cost: (lvl) => Math.floor(10 * Math.pow(lvl, 3)),
   },
   speed: {
     lvl: 1,
     val: 1,
-    next: (lvl) => lvl + 1,
-    cost: (lvl) => Math.floor(10 * Math.pow(lvl, 2.2)),
+    next: (lvl) => Math.floor((lvl * 0.1)*10)/10 + 1,
+    cost: (lvl) => Math.floor(Math.pow(lvl/10+1, 5)),
   }
 }
