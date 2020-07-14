7115132e5a8948d67193278631a24e9e247d8f1c
Aaron Whipple <argwhip@gmail.com>
2020-07-14 12:23:56 -0700

    Increase upgrade cost

--- gameObjects/Stats.js
+++ gameObjects/Stats.js
@@ -3,12 +3,12 @@ export default {
     lvl: 1,
     val: 1,
     next: (lvl) => lvl + 1,
-    cost: (lvl) => 10 * Math.pow(lvl, 2),
+    cost: (lvl) => Math.floor(10 * Math.pow(lvl, 2.2)),
   },
   speed: {
     lvl: 1,
     val: 1,
     next: (lvl) => lvl + 1,
-    cost: (lvl) => 10 * Math.pow(lvl, 2),
+    cost: (lvl) => Math.floor(10 * Math.pow(lvl, 2.2)),
   }
 }
