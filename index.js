edbebba65a1e584e0d8c884c90b84cb67ec2064d
Aaron Whipple <argwhip@gmail.com>
2020-08-03 14:24:58 -0700

    Figured out ipad bug and commented related code for posterity

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import Game from './Game.js';
 
+// async function getGameJS() {
+//   let y = await fetch('./Game.js');
+//   y = await y.json();
+//   return y;
+// }
+// Game$$Game().then(Game => {
+
 window.onload = function() {
   var game = new Game();
   game.start();
