72afad095aaf715b3337849191f5e93b61b6004b
Aaron Whipple <argwhip@gmail.com>
2020-08-06 16:03:23 -0700

    Remove release script

--- release.sh
+++
@@ -1,8 +0,0 @@
-git co release
-git reset --hard HEAD~1
-git merge master
-echo "base-command.whipple.life" > CNAME
-git add CNAME
-git commit -m "Update CNAME for release"
-git push release release
-git co master
